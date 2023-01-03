171c9b3ad7a33b4b4907cca2c1c30a7bb994a5bd
youshaojun <youshaojunde@163.com>
2023-01-03 18:10:09 +0800

    feat: mybatis

---
+++ yousj-datasource/src/main/java/top/yousj/datasource/config/MybatisConfig.java
@@ -0,0 +1,21 @@
+package top.yousj.datasource.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import top.yousj.datasource.interceptor.PerformanceInterceptor;
+
+/**
+ * @author yousj
+ * @since 2023-01-03
+ */
+@Configuration
+public class MybatisConfig {
+
+	@Bean
+	@Profile({"dev", "test"})
+	public PerformanceInterceptor performanceInterceptor() {
+		return new PerformanceInterceptor();
+	}
+
+}
