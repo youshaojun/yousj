95b85b195db33b46f4189fa2fd4ad91ace52fac8
youshaojun <youshaojunde@163.com>
2023-01-08 00:01:35 +0800

    feat: swagger版本兼容

--- yousj-swagger/src/main/java/top/yousj/swagger/SwaggerConfig.java
+++ yousj-swagger/src/main/java/top/yousj/swagger/SwaggerConfig.java
@@ -2,12 +2,20 @@ package top.yousj.swagger;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author yousj
@@ -19,19 +27,55 @@ import java.util.List;
 @Profile({"dev", "test"})
 public class SwaggerConfig {
 
-    public SwaggerConfig(List<Docket> dockets) {
-        dockets.forEach(e -> log.debug(" load swagger group [" + e.getGroupName() + "]"));
-    }
+	static {
+		System.setProperty("spring.mvc.pathmatch.matching-strategy", "ant_path_matcher");
+	}
 
-    @Configuration
-    public static class DefaultDocket {
+	public SwaggerConfig(List<Docket> dockets) {
+		dockets.forEach(e -> log.debug(" load swagger group [" + e.getGroupName() + "]"));
+	}
 
-        @Bean
-        public Docket docket() {
-            SwaggerGroups.SwaggerGroup swaggerGroup = new SwaggerGroups.SwaggerGroup();
-            swaggerGroup.setGroupName("a.全部");
-            return SwaggerDocket.of(swaggerGroup);
-        }
-    }
+	@Bean
+	public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+		return new BeanPostProcessor() {
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+					customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+				}
+				return bean;
+			}
+
+			private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+				List<T> copy = mappings.stream()
+					.filter(mapping -> mapping.getPatternParser() == null)
+					.collect(Collectors.toList());
+				mappings.clear();
+				mappings.addAll(copy);
+			}
+
+			@SuppressWarnings("unchecked")
+			private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+				try {
+					Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+					field.setAccessible(true);
+					return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+		};
+	}
+
+	@Configuration
+	public static class DefaultDocket {
+
+		@Bean
+		public Docket docket() {
+			SwaggerGroups.SwaggerGroup swaggerGroup = new SwaggerGroups.SwaggerGroup();
+			swaggerGroup.setGroupName("a.全部");
+			return SwaggerDocket.of(swaggerGroup);
+		}
+	}
 
 }
