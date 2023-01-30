2578641d9c3a5645a4ded2684dee779db14d9a2d
youshaojun <youshaojunde@163.com>
2023-01-30 17:17:48 +0800

    feat: datasource

--- yousj-datasource/src/main/java/top/yousj/datasource/mapper/CustomizeBaseMapper.java
+++ yousj-datasource/src/main/java/top/yousj/datasource/mapper/CustomizeBaseMapper.java
@@ -1,6 +1,9 @@
 package top.yousj.datasource.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @author yousj
@@ -11,5 +14,10 @@ public interface CustomizeBaseMapper<T> extends BaseMapper<T> {
 	int replaceInto(T entity);
 
 	int insertOnDuplicateKeyUpdate(T entity);
+
+	default T getOneWithMain(@Param(Constants.WRAPPER) LambdaQueryWrapper<T> lambdaQueryWrapper) {
+		lambdaQueryWrapper.first("/*main*/");
+		return selectOne(lambdaQueryWrapper);
+	}
 
 }
