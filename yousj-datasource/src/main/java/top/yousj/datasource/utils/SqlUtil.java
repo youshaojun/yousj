455e52e2048fed9c24e631aec1a01d024dd4441b
youshaojun <youshaojunde@163.com>
2023-02-13 20:54:52 +0800

    feat: with main

--- yousj-datasource/src/main/java/top/yousj/datasource/utils/SqlUtil.java
+++ yousj-datasource/src/main/java/top/yousj/datasource/utils/SqlUtil.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SqlUtil {
 
-	public static final String MAIN = "/**main**/";
+	public static final String MAIN = "/* ShardingSphere hint: dataSourceName=main */";
 
 	public static String getColumnScript(TableInfo tableInfo) {
 		return SqlScriptUtils.convertTrim(tableInfo.getAllInsertSqlColumnMaybeIf(null), StringPool.LEFT_BRACKET, StringPool.RIGHT_BRACKET, null, StringPool.COMMA);
