6439f02a2eaaca103d9096bd87dff6152fc3c88b
youshaojun <youshaojunde@163.com>
2023-02-15 17:35:04 +0800

    feat: log

--- yousj-log/src/main/java/top/yousj/log/aop/LogPointHandler.java
+++ yousj-log/src/main/java/top/yousj/log/aop/LogPointHandler.java
@@ -4,6 +4,7 @@ package top.yousj.log.aop;
  * @author yousj
  * @since 2023-01-04
  */
+@FunctionalInterface
 public interface LogPointHandler {
 
 	void handle(RequestLog requestLog) throws Exception;
