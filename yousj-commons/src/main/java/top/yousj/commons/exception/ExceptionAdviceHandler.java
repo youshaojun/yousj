cc16d63d46e512d5385733bced74cc77cbc3a123
youshaojun <youshaojunde@163.com>
2023-02-16 10:33:00 +0800

    feat: exception handler

--- yousj-commons/src/main/java/top/yousj/commons/exception/ExceptionAdviceHandler.java
+++ yousj-commons/src/main/java/top/yousj/commons/exception/ExceptionAdviceHandler.java
@@ -2,13 +2,13 @@ package top.yousj.commons.exception;
 
 import top.yousj.commons.entity.R;
 
-
 /**
  * 异常处理
  *
  * @author yousj
  * @since 2023-01-05
  */
+@FunctionalInterface
 public interface ExceptionAdviceHandler {
 
 	R<String> handle(Exception ex);
