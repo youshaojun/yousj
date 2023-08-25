f93048b6832499ab3bfe416d75593690948a86af
yousj <youshaojunde@163.com>
2023-08-25 13:13:20 +0800

    fix: log point

--- yousj-log/src/main/java/top/yousj/log/aop/LogPointMethodInterceptor.java
+++ yousj-log/src/main/java/top/yousj/log/aop/LogPointMethodInterceptor.java
@@ -8,6 +8,7 @@ import org.slf4j.MDC;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
@@ -82,13 +83,17 @@ public class LogPointMethodInterceptor implements MethodInterceptor {
                 requestLog.setUserAgent(request.getHeader(HttpHeaders.USER_AGENT));
                 requestLog.setIp(IpUtil.getIpAddr(request));
                 Map<String, Object> requestParameterMap = new HashMap<>();
-                for (Object arg : pjp.getArguments()) {
-                    if (skip(arg)) {
-                        continue;
+                Arrays.stream(pjp.getArguments()).filter(e -> !skip(e)).forEach(e ->  requestParameterMap.putAll(JsonUtil.fromJson(JsonUtil.toJson(e), Map.class)));
+                Map<String, String[]> parameterMap = request.getParameterMap();
+                if (!CollectionUtils.isEmpty(parameterMap)) {
+                    for (String paramName : parameterMap.keySet()) {
+                        String[] paramValues = parameterMap.get(paramName);
+                        if (paramValues != null && paramValues.length > 0) {
+                            String paramValue = paramValues[0];
+                            requestParameterMap.put(paramName, paramValue);
+                        }
                     }
-                    requestParameterMap.putAll(JsonUtil.fromJson(JsonUtil.toJson(arg), Map.class));
                 }
-                requestParameterMap.putAll(request.getParameterMap());
                 String json = JsonUtil.toJson(requestParameterMap);
                 int length = json.length();
                 Integer limit = logProperties.getAop().getRequestParamsLimit();
