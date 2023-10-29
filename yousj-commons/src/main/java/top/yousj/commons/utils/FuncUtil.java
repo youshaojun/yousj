0da098494106b54528fc85e9b05707d52c65a12a
yousj <youshaojunde@163.com>
2023-10-29 20:38:09 +0800

    feat: toMap

--- yousj-commons/src/main/java/top/yousj/commons/utils/FuncUtil.java
+++ yousj-commons/src/main/java/top/yousj/commons/utils/FuncUtil.java
@@ -4,10 +4,16 @@ import io.vavr.CheckedFunction0;
 import io.vavr.control.Try;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * @author yousj
@@ -16,32 +22,43 @@ import java.util.function.Supplier;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FuncUtil {
 
-	public static <T> T conditionCall(boolean condition, CheckedFunction0<? extends T> supplier) {
-		return condition ? Try.of(supplier).getOrNull() : null;
-	}
+    public static <T> T conditionCall(boolean condition, CheckedFunction0<? extends T> supplier) {
+        return condition ? Try.of(supplier).getOrNull() : null;
+    }
 
-	public static <T> T callIfNotNull(Object o, CheckedFunction0<? extends T> supplier) {
-		return conditionCall(Objects.nonNull(o), supplier);
-	}
+    public static <T> T callIfNotNull(Object o, CheckedFunction0<? extends T> supplier) {
+        return conditionCall(Objects.nonNull(o), supplier);
+    }
 
-	public static <T> void callIfNotNull(T t, Consumer<T> consumer) {
-		if (Objects.nonNull(t)) {
-			Try.run(() -> consumer.accept(t));
-		}
-	}
+    public static <T> void callIfNotNull(T t, Consumer<T> consumer) {
+        if (Objects.nonNull(t)) {
+            Try.run(() -> consumer.accept(t));
+        }
+    }
 
-	public static void run(boolean condition, Runnable runnable) {
-		if (condition) {
-			Try.runRunnable(runnable);
-		}
-	}
+    public static void run(boolean condition, Runnable runnable) {
+        if (condition) {
+            Try.runRunnable(runnable);
+        }
+    }
 
-	public static void run(boolean condition, Runnable runnable, Runnable orElse) {
-		Try.runRunnable(condition ? runnable : orElse);
-	}
+    public static void run(boolean condition, Runnable runnable, Runnable orElse) {
+        Try.runRunnable(condition ? runnable : orElse);
+    }
 
-	public static <T> T supplier(boolean condition, Supplier<T> supplier, Supplier<T> orElse) {
-		return Try.ofSupplier(condition ? supplier : orElse).getOrNull();
-	}
+    public static <T> T supplier(boolean condition, Supplier<T> supplier, Supplier<T> orElse) {
+        return Try.ofSupplier(condition ? supplier : orElse).getOrNull();
+    }
+
+    public static <T, K> Map<K, T> toMap(List<T> list, Function<? super T, ? extends K> keyMapper) {
+        return toMap(list, keyMapper, e -> e);
+    }
+
+    public static <T, K, U> Map<K, U> toMap(List<T> list, Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
+        if (CollectionUtils.isEmpty(list)) {
+            return Collections.emptyMap();
+        }
+        return list.stream().collect(Collectors.toMap(keyMapper, valueMapper, (k1, k2) -> k1));
+    }
 
 }
